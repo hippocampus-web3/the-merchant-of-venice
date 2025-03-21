c406697e5a873e0254403d9faa59558f25cc6c7f
hippocampus-web3 <hippocampus.web3@proton.me>
2025-03-21 14:09:18 +0100

    Enable tip system

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import dotenv from "dotenv";
 import { getOrdersFromTwitterLast24hWithHastagh } from "./controllers/twitter.controller.js";
 import cron from "node-cron";
 import { getSuggestedPositionsBasedOnVeniceOutput } from "./controllers/venice.controller.js";
-import { closePosition, getVaultUsdValue, marketOrder } from "./services/hyperliquid.js";
+import { closePosition, getPositionFillByOid, getVaultUsdValue, marketOrder } from "./services/hyperliquid.js";
 import { mapHorizonMilliseconds, mapSizePercentage } from "./constants.js";
+import { sendTipInRune } from "./services/wallet.js";
 
 dotenv.config();
 
 async function merchantWork() {
 
-  if (!process.env.VAULT_ADDRESS) {
-    throw Error ('VAULT_ADDRESS is not defined')
+  if (!process.env.VAULT_ADDRESS && !process.env.LEADER_ADDRESS) {
+    throw Error ('VAULT_ADDRESS and LEADER_ADDRESS is not defined')
   }
 
   console.log(`${Date.now()} Executing merchant work...`);
@@ -23,14 +24,29 @@ async function merchantWork() {
   // 3) Open positions in Hyperliquid
   for (const position of suggestedPositions) {
     const sizePercentage = mapSizePercentage[position.size]
-    const result = await getVaultUsdValue(process.env.VAULT_ADDRESS as string)
+    const result = await getVaultUsdValue(process.env.VAULT_ADDRESS as string || process.env.LEADER_ADDRESS as string)
     const amountInDollars = Math.trunc(Number(result) * sizePercentage) // TODO: Minimun size for position 10
     await marketOrder(position.ticker +  '-PERP', amountInDollars > 11 ? amountInDollars : 12, position.direction === 'long' ? true : false, position.horizon)
     // 4) Configure Timeouts to close positions depending on provided horizon
     const timeToDelay = mapHorizonMilliseconds[position.horizon]
     setTimeout(async () => {
-      await closePosition(position.ticker +  '-PERP')
-    }, timeToDelay); // TODO use correct parametrization for cloaseDelay
+      try {
+        const oid = await closePosition(position.ticker +  '-PERP')
+
+        if (oid) {
+          const positionInfo = await getPositionFillByOid(oid)
+          // Get PNL by position
+          const pnl = Number(positionInfo?.closedPnl) || 0
+          const tipAmountInDollars = pnl > 2 ? pnl * 0.1 : 0 // Tip 10% of PNL if bigger than $1
+          if (tipAmountInDollars > 0 && position.tip) {
+            const hash = await sendTipInRune(tipAmountInDollars, position.tip)
+            console.info("Sending tip to", position.tip, "with amount in dollars", tipAmountInDollars, "hash", hash);
+          }
+        }
+      } catch(e) {
+        console.error('Error closing position or sending tip', e)
+      }
+    }, timeToDelay);
   }
 }
 
--- src/services/venice.ts
+++ src/services/venice.ts
@@ -1,10 +1,16 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
 import { MerchantPosition, VeniceResponse } from "../types.js";
+import { readFile } from 'fs/promises';
 
 dotenv.config();
 
 const BASE_URL = "https://api.venice.ai/api/v1"
+
+const loadVeniceResponse = async () => {
+  const rawData = await readFile('./src/tests/venice-response.json', 'utf-8');
+  return JSON.parse(rawData);
+};
 
 const client = new OpenAI.OpenAI({
   apiKey: process.env.VENICE_API_KEY,
@@ -15,32 +21,39 @@ const getPromp = (orders: MerchantPosition[]) => {
     return `
         Select a number ${process.env.NUMBER_OF_ORDERS_TO_OPEN || 1} of order(s) that you consider the most profitable from the following list of trading orders:  
 
-        ${orders.map(order => (`Ticker: ${order.ticker}\nDirection: ${order.direction}\nSize: ${order.size}\nTime horizon: ${order.horizon}`))}  
+        ${orders.map(order => (`Ticker: ${order.ticker}\nDirection: ${order.direction}\nSize: ${order.size}\nTime horizon: ${order.horizon}\nTip: ${order?.tip}`))}  
 
-        Your response must be in the following format. Only that response in that format, nothing else. Do not provide any additional information. Make sure to always return at least one order no matter what and strictly follow the format. But always one from the given list of trading orders.  
+        Your response must be in the following format. Only that response in that format, nothing else. Do not provide any additional information. Make sure to always return at least one order no matter what and strictly follow the format. But always one from the given list of trading orders. Tip is an optional field. If there is no value for it, ignore it. Do not choose two positions for the same asset.  
 
         [
             {
                 "ticker": "RUNE",
                 "direction": "short",
                 "size": "medium",
-                "horizon": "8h"
+                "horizon": "8h",
+                "tip: "thor1jqsdv03pp867t98d0kwe0pzl5ks6q0f9fvf3ha"
             }
         ]  
     `
 } 
 
 export async function getSugestionsUsingVenice(orders: MerchantPosition[]): Promise<VeniceResponse> {
-    try {
-        const promp = getPromp(orders)
-        const chatCompletion = await client.chat.completions.create({
-            messages: [{ role: 'user', content: promp }],
-            model: 'llama-3.3-70b',
-        });
-        return chatCompletion as VeniceResponse
-    } catch(e) {
-        console.error(e)
-        throw e
+    if (process.env.VENICE_API_KEY) {
+        try {
+            const promp = getPromp(orders)
+            const chatCompletion = await client.chat.completions.create({
+                messages: [{ role: 'user', content: promp }],
+                model: 'llama-3.3-70b',
+            });
+            return chatCompletion as VeniceResponse
+        } catch(e) {
+            console.error(e)
+            throw e
+        }
+    } else {
+        console.warn('VENICE_API_KEY not set, skipping search and working with mock data')
+        const veniceResponse = await loadVeniceResponse()
+        return veniceResponse
     }
 }
 
